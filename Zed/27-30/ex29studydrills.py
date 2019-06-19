210e0e5863ebbdb7b3e6b3ddddc99bd94169e7db
sdmiller <sierraallinone@gmail.com>
2019-06-19 16:37:13 -0400

    Adding exercises 27-30

---
+++ Zed/27-30/ex29studydrills.py
@@ -0,0 +1,13 @@
+# 1. The if prints the included statement if returned True. 
+# 2. The code needs to be indented to make it known that the print statement is included with that if statement, it's a part of it. 
+# 3. If it's not indented, it isn't included with the if statement and will print regardless of the truth of the if statement. 
+
+# 4. 
+people = 20
+cats = 30
+dogs = 15
+
+if people != cats:
+        print("People aren\'t cats")
+
+# 5. it will change the truth of the arguments. 
